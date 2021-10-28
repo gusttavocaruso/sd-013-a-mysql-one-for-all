c61a0c10ea8907c4ee9e2bbacc929d0cdb0f5c5b
Murilo Rainho <murilorainho01@gmail.com>
2021-10-28 18:00:51 -0300

    realizando requisito 10

---
+++ desafio10.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+DELIMITER $$;
+
+CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE result INT;
+	SELECT COUNT(*) FROM SpotifyClone.historico_de_reproducoes hr
+    WHERE hr.usuario_id = user_id
+    INTO result;
+    RETURN result;
+END $$;
+
+DELIMITER ;
