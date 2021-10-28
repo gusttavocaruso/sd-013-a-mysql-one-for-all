9bbd045e1b8278a666c3dd9ec970ca40792e259b
Murilo Rainho <murilorainho01@gmail.com>
2021-10-28 17:59:37 -0300

    realizando requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+USE SpotifyClone;
+
+CREATE VIEW historico_reproducao_usuarios AS
+	SELECT u.nome `usuario`,
+    m.titulo `nome`
+    FROM SpotifyClone.historico_de_reproducoes hr
+    INNER JOIN SpotifyClone.usuarios u ON u.usuario_id = hr.usuario_id
+    INNER JOIN SpotifyClone.musicas m ON m.musica_id = hr.musica_id
+    ORDER BY `usuario`, `nome`;
