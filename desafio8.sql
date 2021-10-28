b89c51d294ff2f807d49b83d31373d4ce39f3078
Murilo Rainho <murilorainho01@gmail.com>
2021-10-28 18:00:31 -0300

    realizando requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+USE SpotifyClone;
+DELIMITER $$;
+
+CREATE TRIGGER trigger_usuario_delete
+BEFORE DELETE ON usuarios
+FOR EACH ROW
+BEGIN
+	DELETE FROM usuarios_artistas ua
+    WHERE OLD.usuario_id = ua.usuario_id;
+    DELETE FROM historico_de_reproducoes hr
+    WHERE OLD.usuario_id = hr.usuario_id;
+END $$;
+
+DELIMITER ;
